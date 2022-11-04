9117562f07b3eec519f966ad869fd2953f6625e4
ISMAIL DINAR <sm41l.4796@gmail.com>
2022-11-04 21:28:39 +0100

    4 files modified

--- projects/downgraded-ng12/src/lib/downgraded-ng12.module.ts
+++ projects/downgraded-ng12/src/lib/downgraded-ng12.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
+import { ApplicationRef, DoBootstrap, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { DowngradedNg12Component } from './downgraded-ng12.component';
 
 @NgModule({
-  declarations: [DowngradedNg12Component],
-  exports: [DowngradedNg12Component],
-  entryComponents: [DowngradedNg12Component],
-  imports: [BrowserModule],
+    declarations: [DowngradedNg12Component],
+    exports: [DowngradedNg12Component],
+    imports: [BrowserModule]
 })
-export class DowngradedNg12Module {}
+export class DowngradedNg12Module implements DoBootstrap {
+    public ngDoBootstrap(appRef: ApplicationRef): void {
+    }
+}
